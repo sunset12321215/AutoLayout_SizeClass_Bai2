83382764965a509db4bc89e6e13a9d13b9f3f0c3
HoaLT-D1 <hoalt-d1@Cuong-Mac.local>
2019-06-13 09:40:08 +0700

    First Commit

---
+++ AutoLayout_SizeClass_Bai2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayout_SizeClass_Bai2
+//
+//  Created by HoaLT-D1 on 6/13/19.
+//  Copyright © 2019 HoaLT-D1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
